356589455130702f1a31546a91306db1efe4d294
Vortebo <64214314+Vortebo@users.noreply.github.com>
2026-03-20 14:33:32 -0500

    almost all 8-bit lds

--- lowlevel/op_funcs.py
+++ lowlevel/op_funcs.py
@@ -4,6 +4,7 @@ from lowlevel.memory import memory
 from lowlevel.timer import timer
 from lowlevel.rom_handler import rom
 
+# Jumps
 def jp_addr():
     dest = rom.get() + rom.get()
     rg.pc.set(dest)
@@ -11,28 +12,40 @@ def jp_addr():
 def jr():
     jp_addr()
 
-def ld_addr_a(dest,inc=0,elaps=8): # ld [bc], a
+# 8-bit loads
+def ld_addr_reg(dest,src,inc=0): # ld [bc], a
     addr = dest.hi.hget() + dest.lo.hget()
-    memory.set(addr,rg.AF.hi.hget())
+    memory.set(addr,src.hget())
     if inc==1:
         rg.HL.inc()
     elif inc==-1:
         rg.HL.dec()
-    timer.tick(elaps)
-def ld_a_addr(dest,inc=0,elaps=8): # ld a, [bc]
-    addr = dest.hi.hget() + dest.lo.hget()
-    rg.AF.hi.set(memory.get(addr))
+    timer.tick(8)
+def ld_reg_addr(dest,src,inc=0): # ld a, [bc]
+    addr = src.hi.hget() + src.lo.hget()
+    dest.set(memory.get(addr))
     if inc==1:
         rg.HL.inc()
     elif inc==-1:
         rg.HL.dec()
-    timer.tick(elaps)
-def ld_reg_arg(dest,elaps=8):          # ld b, n8
+    timer.tick(8)
+def ld_reg_arg(dest):          # ld b, n8
     src = rom.get()
     dest.set(src)
-    timer.tick(elaps)
-def ld_addr_arg(): # ld [bc], a
+    timer.tick(8)
+def ld_addr_arg(): 
     addr = rg.HL.hi.hget() + rg.HL.lo.hget()
     src = rom.get()
     memory.set(addr,src)
     timer.tick(12)
+def ld_reg_reg(dest,src):          # ld b, b
+    dest.set(src.hget())
+    timer.tick(4)
+def ld_addr_a():
+    addr = rom.get() + rom.get()
+    memory.set(addr,rg.AF.hi.hget())
+    timer.tick(16)
+def ld_a_addr():
+    addr = rom.get() + rom.get()
+    rg.AF.hi.set(memory.get(addr))
+    timer.tick(16)
