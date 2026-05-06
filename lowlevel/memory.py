c52320b60a446e2f98f2b5691abf14b62425782c
Vortebo <64214314+Vortebo@users.noreply.github.com>
2026-05-06 12:32:01 -0500

    well that was a little silly of me

--- lowlevel/memory.py
+++ lowlevel/memory.py
@@ -22,12 +22,12 @@ class Membank:
         wram2size = 4096 # switchable bank 1 - 7 in CGB
         wram2 = bytearray(wram2size)
 
-        # echo ram goes here
+        echosize = 7680 # TODO: make it actually echo
 
         oamsize = 160
         oam = bytearray(oamsize)
 
-        # not usable
+        nousesize = 96
 
         ioregsize = 128
         ioreg = bytearray(ioregsize)
@@ -38,13 +38,15 @@ class Membank:
         iesize = 1
         ie = bytearray(iesize)
 
-        self.sizes = [bank0size,bankNsize,vramsize,eramsize,wram1size,wram2size,oamsize,ioregsize,hramsize,iesize]
-        self.banks = [bank0,bankN,vram,eram,wram1,wram2,oam,ioreg,hram,ie]
+        self.sizes = [bank0size,bankNsize,vramsize,eramsize,wram1size,wram2size,echosize,oamsize,nousesize,ioregsize,hramsize,iesize]
+        self.banks = [bank0,bankN,vram,eram,wram1,wram2,None,oam,None,ioreg,hram,ie]
 
     def address_adjust(self, address):
         loc = HexValue(address).iget()
+        print(f'Translated address is {loc}')
         start=0
         for i in range(len(self.sizes)):
+            print(f'Bank {i} starts at {start}')
             if start <= loc < self.sizes[i] + start:
                 return loc - start, i
             start += self.sizes[i]
@@ -52,7 +54,7 @@ class Membank:
     
     def get(self, address):
         loc, bank = self.address_adjust(address)
-        print(loc,bank)
+        print(f'Equivalent is {loc} in bank {bank}')
         if bank < 2:
             return rom.get(address)
         return HexValue(self.banks[bank][loc:loc+1].hex())
@@ -61,6 +63,7 @@ class Membank:
         return rom.getrange(length)
 
     def set(self, address, value):
+        print(f'Attempting to set {value} at address 0x{address}')
         loc, bank = self.address_adjust(address)
         self.banks[bank][loc] = value
 
